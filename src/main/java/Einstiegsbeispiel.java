c5f262ad5535c5309b9d887da823c64c3ecaa358
Markus Messner <markusmes@edu.aau.at>
2022-10-08 22:09:28 +0200

    'implementation'

--- src/main/java/Einstiegsbeispiel.java
+++ src/main/java/Einstiegsbeispiel.java
@@ -1,2 +1,48 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+
+
+
 public class Einstiegsbeispiel {
+
+    public static void main(String[] args) {
+
+        int [] arr = {1,1,1,2,2,2,3,3,3,3,4,4,5};
+
+        System.out.println(getRepetitions(arr).get(5));
+
+    }
+
+
+
+
+
+
+
+
+
+    public static HashMap getRepetitions(int[] arr){
+
+        HashMap<Integer, Integer> numbers = new HashMap<Integer, Integer>();
+
+
+
+        for (int i = 0; i < arr.length  ; i++) {
+
+            if(numbers.containsKey(arr[i])){
+
+              numbers.computeIfPresent(arr[i], (k,v) -> v+1);
+
+            }else{
+
+                numbers.put(arr[i], 1);
+            }
+
+
+        }
+
+        return numbers;
+
+    }
 }
