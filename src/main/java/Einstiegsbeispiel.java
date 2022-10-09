8c6a0bcb3cf49c767c8b28f8dbabff9c443fea21
MarkusMessner <markusmes@edu.aau.at>
2022-10-09 18:21:59 +0200

    Implementation Tests

--- src/main/java/Einstiegsbeispiel.java
+++ src/main/java/Einstiegsbeispiel.java
@@ -1,39 +1,29 @@
-import java.util.Arrays;
 import java.util.HashMap;
-
-
+import java.util.Map;
 
 
 public class Einstiegsbeispiel {
 
     public static void main(String[] args) {
 
-        int [] arr = {1,1,1,2,2,2,3,3,3,3,4,4,5};
+        int[] arr = {1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5};
 
 
     }
 
 
-
-
-
-
-
-
-
-    public  HashMap getRepetitions(int[] arr){
+    public static HashMap getRepetitions(int[] arr) throws Exception {
 
         HashMap<Integer, Integer> numbers = new HashMap<Integer, Integer>();
 
 
+        for (int i = 0; i < arr.length; i++) {
 
-        for (int i = 0; i < arr.length  ; i++) {
+            if (numbers.containsKey(arr[i])) {
 
-            if(numbers.containsKey(arr[i])){
+                numbers.computeIfPresent(arr[i], (k, v) -> v + 1);
 
-              numbers.computeIfPresent(arr[i], (k,v) -> v+1);
-
-            }else{
+            } else {
 
                 numbers.put(arr[i], 1);
             }
@@ -41,7 +31,31 @@ public class Einstiegsbeispiel {
 
         }
 
+        //Check ob sich Sequenzen wiederholen
+        if(!checkIfSequenzesRepeat(numbers)){
+            throw new Exception();
+        }
+
+
         return numbers;
 
+    }
+    //true wenn sich die Reihe wiederholt
+    public static boolean checkIfSequenzesRepeat(HashMap<Integer, Integer> mymap) {
+
+
+        for (Map.Entry<Integer, Integer> value : mymap.entrySet()) {
+
+            Integer i = value.getValue();
+
+            if (!value.getValue().equals(1)) {
+
+                return true;
+            }
+
+
+        }
+
+        return false;
     }
 }
